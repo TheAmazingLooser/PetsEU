47f3bc7e967aa965306021b36584267e98702a45
YoungFlyme <youngflyme@web.de>
2023-03-31 11:18:57 +0200

    Added default pets controller and class for API

---
+++ PetsEU_API/Controllers/PetsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetsEU_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetsController : Controller
+    {
+        [HttpGet(Name = "GetPets")]
+        public IEnumerable<Pets> Get(int? id)
+        {
+            // TODO: Implement
+            return null;
+        }
+    }
+}
---
+++ PetsEU_API/Pets.cs
@@ -0,0 +1,11 @@
+﻿namespace PetsEU_API
+{
+    public class Pets
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PostedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public List<string> Base64ImageData { get; set; }
+    }
+}
